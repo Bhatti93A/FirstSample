82b1b6163269ed37052eee2b90b1f103561dbcdb
Abhay <abhaybhatti2011@yahoo.com>
2019-01-21 17:07:21 +0530

    Adding changes

--- src/main/java/com/qaagility/javaee/HelloServlet.java
+++ src/main/java/com/qaagility/javaee/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloServlet
+ * Hi This is a Servlet implementation class HelloServlet
  */
 @WebServlet("/helloServlet")
 public class HelloServlet extends HttpServlet {
